50de87668b6da2146aa83beb322ce42ca3d4d462
AayushNair10 <aayushnair204@gmail.com>
2025-09-16 23:31:52 -0400

    Update website

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,33 +1,31 @@
 const projectItems = [
-    {
-      name: "C++ Algorithmic Order Matching Engine",
-      description:
-        "Online platform matches hostel roommates by preferences. Features: profiles, search, messaging, matching, verification, reviews, privacy, mobile support.",
-      gitLink: "https://github.com/AayushNair10/Algorithmic-Order-Matching-Engine",
-      urlLink: "",
-      skills: [],
-    },
+  {
+    name: "C++ Algorithmic Order Matching Engine",
+    description:
+      "Developed an ultra-low-latency algorithmic trading engine in C++, handling order books, efficiently matching buy and sell orders, and executing trades in real time.",
+    gitLink: "https://github.com/AayushNair10/Algorithmic-Order-Matching-Engine",
+    urlLink: "",
+    skills: [],
+  },
 
-    {
-      name: "RoboFriends",
-      description:
-        "This React project displays a list of robots fetched from an API, with a search feature that filters the robots based on the user's input.",
-      gitLink: "https://github.com/AayushNair10/robo-friends",
-      urlLink: "robo-friends-blue.vercel.app",
-      skills: [],    
+  {
+    name: "BentoDBMS: A Simplified SQL Database Engine in C++",
+    description:
+      "Built a lightweight SQL-compatible database engine supporting core operations like CREATE, INSERT, SELECT, UPDATE, and JOIN. Implemented custom file storage and B+ Tree indexing to simulate internal DBMS workflows and deepen understanding of database systems.",
+    gitLink: "https://github.com/AayushNair10/robo-friends",
+    urlLink: "robo-friends-blue.vercel.app",
+    skills: [],
+  },
+  {
+    name: "VirtuVoyage: An AI-Powered Travel Companion",
+    description:
+      "Built an AI-powered platform for smart trip planning and immersive 360° monument tours. Implemented itinerary generation with clustering and shortest-path algorithms, and a conversational assistant using RAG and multimodal AI. Designed a modular, scalable backend for easy content expansion.",
+    gitLink: "https://doi.org/10.1109/ICEI64305.2024.10912340",
+    urlLink: "https://doi.org/10.1109/ICEI64305.2024.10912340",
+    skills: [],
 
-    },
+  },
 
-    {
-      name: "A Novel RoPE-Enhanced Informer Approach for Accurate Remaining Useful Life Prediction of Lithium-Ion Batteries",
-      description:
-        "IEEE publication",
-      gitLink: "https://doi.org/10.1109/ICEI64305.2024.10912340",
-      urlLink: "https://doi.org/10.1109/ICEI64305.2024.10912340",
-      skills: [],    
+];
 
-    },
-
-  ];
-
-  export default projectItems;
+export default projectItems;
